fc93bff928a5e74f78e2c775b3e087e11b096893
harteagabran <143139324+harteagabran@users.noreply.github.com>
2024-02-25 19:23:33 -0600

    Created Base Menus to Make Databases
    Created base menus for both units and weapons, need to create entities and start the database filled with items

---
+++ src/main/java/StartProgram.java
@@ -0,0 +1,17 @@
+
+/**
+ * @author Halmar Arteaga - harteagabran
+ * CIS175 - Spring 2024
+ * Feb 25, 2024
+ */
+public class StartProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
